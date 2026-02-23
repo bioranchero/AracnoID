270762a397f17205b1796f49cbf15a46b28c0b33
bioranchero <biologiasitioweb@gmail.com>
2026-02-23 15:00:31 -0800

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ with tab_app:
         if st.button("🚀 Analizar ahora"):
             with st.spinner("Procesando imagen..."):
                 # Carga simple, la magia la hará el requirements.txt que pusimos arriba
-                    try:
+                try:
                     modelo = tf.keras.models.load_model('modelo_aracnoid.h5', compile=False)
                 except Exception as e:
     st.error(f"Error al cargar el modelo: {e}")
